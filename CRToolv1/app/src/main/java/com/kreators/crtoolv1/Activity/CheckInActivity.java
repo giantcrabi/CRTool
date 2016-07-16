ab8158885ccc0e0a110c15f9676b9e6aa3c2cd7e
Gian Sebastian <giantcrabi@gmail.com>
2016-07-16 17:34:42 +0700

    Design Interface v1.6 16-07-2016

--- CRToolv1/app/src/main/java/com/kreators/crtoolv1/Activity/CheckInActivity.java
+++ CRToolv1/app/src/main/java/com/kreators/crtoolv1/Activity/CheckInActivity.java
@@ -1,46 +1,296 @@
 package com.kreators.crtoolv1.Activity;
 
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.content.Intent;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.util.Pair;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
-import com.kreators.crtoolv1.Fragment.CheckInMainFragment;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResult;
+import com.google.android.gms.location.LocationSettingsStates;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
 import com.kreators.crtoolv1.Fragment.Dialog.SelectOutletDialogFragment;
 import com.kreators.crtoolv1.R;
 
-public class CheckInActivity extends AppCompatActivity implements CheckInMainFragment.CheckInMainListener{
+import java.util.ArrayList;
+import java.util.List;
+
+public class CheckInActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        LocationListener,
+        SelectOutletDialogFragment.MyDialogFragmentListener {
+
+    public static final String TAG = CheckInActivity.class.getSimpleName();
+
+    private static final int PERMISSION_LOCATION_REQUEST_CODE = 1;
+    private final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
+    protected static final int REQUEST_CHECK_SETTINGS = 0x1;
+
+    private final static int EARTH_RADIUS = 6371;
+    private final static double CHECK_IN_RADIUS = 0.5;
+
+    private GoogleApiClient googleApiClient;
+    private LocationRequest mLocationRequest;
+    private double curLat;
+    private double curLon;
+
+    private List<Pair<Pair<Double,Double>,String>> listOutlet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_check_in);
 
-        if (findViewById(R.id.checkInActivity) != null) {
+        if ( ContextCompat.checkSelfPermission( this, android.Manifest.permission.ACCESS_COARSE_LOCATION ) != PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
 
-            // However, if we're being restored from a previous state,
-            // then we don't need to do anything and should return or else
-            // we could end up with overlapping fragments.
-            if (savedInstanceState != null) {
-                return;
-            }
-
-            // Create a new Fragment to be placed in the activity layout
-            CheckInMainFragment checkInMainFragment= new CheckInMainFragment();
-
-            // Add the fragment to the 'fragment_container' FrameLayout
-            getSupportFragmentManager().beginTransaction().add(R.id.checkInActivity, checkInMainFragment).commit();
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_COARSE_LOCATION, android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSION_LOCATION_REQUEST_CODE);
         }
 
+        googleApiClient = new GoogleApiClient.Builder(this).
+                addConnectionCallbacks(this).
+                addOnConnectionFailedListener(this).
+                addApi(LocationServices.API).
+                build();
+
+        // Create the LocationRequest object
+        mLocationRequest = LocationRequest.create()
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                .setInterval(60 * 1000)        // 60 seconds, in milliseconds
+                .setFastestInterval(1 * 1000); // 1 second, in milliseconds
+
+        listOutlet = new ArrayList<Pair<Pair<Double,Double>,String>>();
+
+        listOutlet.add(new Pair<Pair<Double,Double>,String>(new Pair<Double,Double>(-7.280583, 112.780866), "A"));
+        listOutlet.add(new Pair<Pair<Double,Double>,String>(new Pair<Double,Double>(-7.284308, 112.783367), "B"));
+        listOutlet.add(new Pair<Pair<Double,Double>,String>(new Pair<Double,Double>(-7.285142, 112.783735), "C"));
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_LOCATION_REQUEST_CODE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
 
-    public void onSelectOutletButtonClick() {
-        final SelectOutletDialogFragment SO = new SelectOutletDialogFragment();
-        SO.show(getFragmentManager(),"Select Outlet");
+                } else {
+                    Toast.makeText(this, "Need your location!", Toast.LENGTH_SHORT).show();
+                }
+
+                break;
+        }
     }
 
-    public void onCheckInButtonClick(){
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (googleApiClient != null) {
+            googleApiClient.connect();
+        }
+        checkLocationSettings();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (googleApiClient.isConnected()) {
+            LocationServices.FusedLocationApi.removeLocationUpdates(googleApiClient, this);
+            googleApiClient.disconnect();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            // Check for the integer request code originally supplied to startResolutionForResult().
+            case REQUEST_CHECK_SETTINGS:
+                switch (resultCode) {
+                    case Activity.RESULT_OK:
+                        if (googleApiClient.isConnected()) {
+                            startLocationUpdates();
+                        }
+                        else {
+                            checkLocationSettings();
+                        }
+                        break;
+                    case Activity.RESULT_CANCELED:
+                        checkLocationSettings();//keep asking if imp or do whatever
+                        break;
+                }
+                break;
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        storeCurrentLocation(location);
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.i(TAG, "Location services suspended. Please reconnect.");
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        if (connectionResult.hasResolution()) {
+            try {
+                // Start an Activity that tries to resolve the error
+                connectionResult.startResolutionForResult(this, CONNECTION_FAILURE_RESOLUTION_REQUEST);
+            } catch (IntentSender.SendIntentException e) {
+                e.printStackTrace();
+            }
+        } else {
+            Log.i(TAG, "Location services connection failed with code " + connectionResult.getErrorCode());
+        }
+    }
+
+    @Override
+    public void onReturnValue(String loc) {
+        Log.d("CHOOSEN OUTLET: ", loc);
         Intent intent = new Intent(this, SalesOutActivity.class);
         startActivity(intent);
     }
+
+    private void checkLocationSettings(){
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
+                .addLocationRequest(mLocationRequest);
+        builder.setAlwaysShow(true); //this is the key ingredient
+        PendingResult<LocationSettingsResult> result =
+                LocationServices.SettingsApi.checkLocationSettings(googleApiClient,
+                        builder.build());
+        result.setResultCallback(new ResultCallback<LocationSettingsResult>() {
+            @Override
+            public void onResult(LocationSettingsResult result) {
+                final Status status = result.getStatus();
+                final LocationSettingsStates state = result.getLocationSettingsStates();
+                switch (status.getStatusCode()) {
+                    case LocationSettingsStatusCodes.SUCCESS:
+                        if (googleApiClient.isConnected()) {
+                            startLocationUpdates();
+                        }
+                        else {
+                            checkLocationSettings();
+                        }
+                        break;
+                    case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                        // Location settings are not satisfied. But could be fixed by showing the user
+                        // a dialog.
+                        try {
+                            // Show the dialog by calling startResolutionForResult(),
+                            // and check the result in onActivityResult().
+                            status.startResolutionForResult(CheckInActivity.this, REQUEST_CHECK_SETTINGS);
+                        } catch (IntentSender.SendIntentException e) {
+                            // Ignore the error.
+                        }
+                        break;
+                    case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                        // Location settings are not satisfied. However, we have no way to fix the
+                        // settings so we won't show the dialog.
+                        break;
+                }
+            }
+        });
+    }
+
+    private void startLocationUpdates() {
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+
+            //lastLocation equivalent to user's current location
+            Location lastLocation = LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
+
+            //LocationServices.FusedLocationApi.requestLocationUpdates(googleApiClient, mLocationRequest, this);
+
+            if(lastLocation == null){
+                LocationServices.FusedLocationApi.requestLocationUpdates(googleApiClient, mLocationRequest, this);
+            } else {
+                storeCurrentLocation(lastLocation);
+            }
+        }
+    }
+
+    private void storeCurrentLocation(Location location) {
+        curLat = location.getLatitude();
+        curLon = location.getLongitude();
+    }
+
+    private void showDialog(List<String> nearestOutlet) {
+        // DialogFragment.show() will take care of adding the fragment
+        // in a transaction.  We also want to remove any currently showing
+        // dialog, so make our own transaction and take care of that here.
+        FragmentTransaction ft = getFragmentManager().beginTransaction();
+        Fragment prev = getFragmentManager().findFragmentByTag("Select Outlet");
+        if (prev != null) {
+            ft.remove(prev);
+        }
+
+        // Create and show the dialog.
+        SelectOutletDialogFragment SO = SelectOutletDialogFragment.newInstance(nearestOutlet);
+        SO.show(ft,"Select Outlet");
+    }
+
+    public void checkIn(View view){
+        //List<Pair<Pair<Double,Double>,String>> nearestOutlet = new ArrayList<Pair<Pair<Double,Double>,String>>();
+        List<String> nearestOutlet= new ArrayList<String>();
+        for(int i = 0; i < listOutlet.size(); i++){
+            double distance = greatCircleDistance(curLat, curLon, listOutlet.get(i).first.first, listOutlet.get(i).first.second);
+            Log.v("Contains: ", "lat: " + listOutlet.get(i).first.first + ", lon: " + listOutlet.get(i).first.second);
+            if(distance <= CHECK_IN_RADIUS){
+                nearestOutlet.add(listOutlet.get(i).second);
+            }
+        }
+        if(nearestOutlet.size() > 0) {
+            if(nearestOutlet.size() > 1){
+                showDialog(nearestOutlet);
+            }
+            else{
+                Log.d("CHOOSEN OUTLET: ", nearestOutlet.get(0).toString());
+                Intent intent = new Intent(this, SalesOutActivity.class);
+                startActivity(intent);
+            }
+        }
+        else {
+            Toast.makeText(this, "There aren't any outlets nearby", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private static double greatCircleDistance(double latNow, double lonNow, double lat, double lon){
+        double latDistance = toRad(lat-latNow);
+        double lonDistance = toRad(lon-lonNow);
+        double a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2) +
+                Math.cos(toRad(latNow)) * Math.cos(toRad(lat)) *
+                        Math.sin(lonDistance / 2) * Math.sin(lonDistance / 2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+        return EARTH_RADIUS * c;
+    }
+
+    private static double toRad(double value) {
+        return value * Math.PI / 180;
+    }
+
 }
--- CRToolv1/app/src/main/java/com/kreators/crtoolv1/Fragment/Dialog/SelectOutletDialogFragment.java
+++ CRToolv1/app/src/main/java/com/kreators/crtoolv1/Fragment/Dialog/SelectOutletDialogFragment.java
@@ -3,45 +3,64 @@ package com.kreators.crtoolv1.Fragment.Dialog;
 
 import android.app.DialogFragment;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SearchView;
 
 import com.kreators.crtoolv1.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * A simple {@link Fragment} subclass.
  */
-public class SelectOutletDialogFragment extends DialogFragment{
+public class SelectOutletDialogFragment extends DialogFragment implements AdapterView.OnItemClickListener {
 
     ArrayAdapter<String> adapter;
-    String[] players = {"Outlet Bumi Marina","Outlet Pakuwon City","Outlet Galaxy Mall","Outlet ITS",
-            "Outlet A", "Outlet B", "Outlet C","Outlet D", "Outlet E",
-            "Outlet F", "Outlet G", "Outlet H","Outlet I", "Outlet J"};
+    ArrayList<String> mOutlets;
 
-    public SelectOutletDialogFragment() {
-        // Required empty public constructor
+//    String[] players = {"Outlet Bumi Marina","Outlet Pakuwon City","Outlet Galaxy Mall","Outlet ITS",
+//            "Outlet A", "Outlet B", "Outlet C","Outlet D", "Outlet E",
+//            "Outlet F", "Outlet G", "Outlet H","Outlet I", "Outlet J"};
+
+    public static SelectOutletDialogFragment newInstance(List<String> nearestOutlet) {
+        SelectOutletDialogFragment f = new SelectOutletDialogFragment();
+
+        // Supply num input as an argument.
+        Bundle args = new Bundle();
+        args.putStringArrayList("listNearestOutlet", (ArrayList) nearestOutlet);
+        f.setArguments(args);
+
+        return f;
     }
 
-    @Nullable
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mOutlets = getArguments().getStringArrayList("listNearestOutlet");
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_select_outlet_dialog,null);
+        View rootView = inflater.inflate(R.layout.fragment_select_outlet_dialog, container, false);
 
         getDialog().setTitle("Select Outlet");
 
-        Button btn = (Button) rootView.findViewById(R.id.btnDismissOutletDialog);
         ListView lv = (ListView) rootView.findViewById(R.id.lvListViewOutlet);
         SearchView sv = (SearchView) rootView.findViewById(R.id.svSearchOutlet);
 
-        adapter = new ArrayAdapter<String>(getActivity(),android.R.layout.simple_list_item_1,players);
+        String[] outlets = new String[mOutlets.size()];
+        outlets = mOutlets.toArray(outlets);
+
+        adapter = new ArrayAdapter<String>(getActivity(),android.R.layout.simple_list_item_1, outlets);
         lv.setAdapter(adapter);
+        lv.setOnItemClickListener(this);
 
         sv.setQueryHint("Search..");
         sv.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
@@ -57,16 +76,19 @@ public class SelectOutletDialogFragment extends DialogFragment{
             }
         });
 
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                dismiss();
-            }
-        });
-
-
         return rootView;
+    }
 
+    @Override
+    public void onItemClick(AdapterView<?> adapter, View arg1, int position, long arg3) {
+        String item = adapter.getItemAtPosition(position).toString();
+        MyDialogFragmentListener activity = (MyDialogFragmentListener) getActivity();
+        activity.onReturnValue(item);
+        dismiss();
+    }
+
+    public interface MyDialogFragmentListener {
+        void onReturnValue(String loc);
     }
 
 }
