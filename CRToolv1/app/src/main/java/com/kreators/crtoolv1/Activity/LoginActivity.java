0b1f1f3656d8595a4cfba8fb2e5085c2b7a14469
imjuanleonard <Julio.Leonard13@mhs.if.its.ac.id>
2016-07-26 03:29:17 +0700

    Can Login
    Need Webservice

--- CRToolv1/app/src/main/java/com/kreators/crtoolv1/Activity/LoginActivity.java
+++ CRToolv1/app/src/main/java/com/kreators/crtoolv1/Activity/LoginActivity.java
@@ -1,23 +1,125 @@
 package com.kreators.crtoolv1.Activity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.kreators.crtoolv1.R;
+import com.kreators.crtoolv1.Utility.LoginConstant;
 
-public class LoginActivity extends AppCompatActivity {
+import java.util.HashMap;
+import java.util.Map;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    EditText editTextEmail;
+    EditText editTextPassword;
+    Button buttonLogin;
+    private boolean loggedIn = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        editTextEmail = (EditText) findViewById(R.id.emailLogin);
+        editTextPassword = (EditText) findViewById(R.id.passwordLogin);
+        buttonLogin = (Button) findViewById(R.id.buttonLogin);
+
+        buttonLogin.setOnClickListener(this);
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //In onresume fetching value from sharedpreference
+        SharedPreferences sharedPreferences = getSharedPreferences(LoginConstant.SHARED_PREF_NAME,Context.MODE_PRIVATE);
+
+        //Fetching the boolean value form sharedpreferences
+        loggedIn = sharedPreferences.getBoolean(LoginConstant.LOGGEDIN_SHARED_PREF, false);
+
+        //If we will get true
+        if(loggedIn){
+            //We will start the Profile Activity
+            Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+            startActivity(intent);
+        }
     }
 
-    public void checkLogin(View view) {
-        Intent intent = new Intent(this, HomeActivity.class);
-        startActivity(intent);
+    private void checkLogin(){
+        //Getting values from edit texts
+        final String email = editTextEmail.getText().toString().trim();
+        final String password = editTextPassword.getText().toString().trim();
+
+        //Creating a string request
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, LoginConstant.LOGIN_URL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        //If we are getting success from server
+                        if(response.equalsIgnoreCase(LoginConstant.LOGIN_SUCCESS)){
+                            //Creating a shared preference
+                            SharedPreferences sharedPreferences = LoginActivity.this.getSharedPreferences(LoginConstant.SHARED_PREF_NAME, Context.MODE_PRIVATE);
+                            //Creating editor to store values to shared preferences
+                            SharedPreferences.Editor editor = sharedPreferences.edit();
+                            //Adding values to editor
+                            editor.putBoolean(LoginConstant.LOGGEDIN_SHARED_PREF, true);
+                            editor.putString(LoginConstant.EMAIL_SHARED_PREF, email);
+                            //Saving values to editor
+                            editor.apply();
+                            //Starting Home activity
+                            Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                            startActivity(intent);
+                        }else{
+                            //If the server response is not success
+                            //Displaying an error message on toast
+                            Toast.makeText(LoginActivity.this, "Invalid username or password", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        //You can handle error here if you want
+                    }
+                }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String,String> params = new HashMap<>();
+                //Adding parameters to request
+                params.put(LoginConstant.KEY_EMAIL, email);
+                params.put(LoginConstant.KEY_PASSWORD, password);
+
+                //returning parameter
+                return params;
+            }
+        };
+
+        //Adding the string request to the queue
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
     }
+
+
+    @Override
+    public void onClick(View v) {
+        checkLogin();
+    }
+
+
+
 
 }
+
